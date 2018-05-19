0e690cdecb77c139202861cfb9ed61533aa98d26
StudentKA <annakapranova77@gmail.com>
2018-05-19 16:18:19 +0300

    Теперь можно добавлять query

--- src/ui/Query.java
+++ src/ui/Query.java
@@ -1,10 +1,32 @@
 package ui;
 
 
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.paint.Color;
+import javafx.scene.control.CheckBox;
+import javafx.scene.layout.HBox;
 
-public class Query  {
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
+
+
+public class Query extends HBox{
+
+
+    private Circle color = new Circle();
+    private Text species = new Text();
+    private CheckBox visibility = new CheckBox();
+
+    public Query(String species, Color color){
+        super(15);
+        this.species.setText(species);
+        this.color.setRadius(7);
+        this.color.setFill(color);
+        this.visibility.setSelected(true);
+        this.visibility.setDisable(false);
+
+        getChildren().addAll(this.species, this.color, visibility);
+
+
+    }
 
 }
--- src/ui/mainStageController.java
+++ src/ui/mainStageController.java
@@ -2,11 +2,17 @@ package ui;
 
 import com.lynden.gmapsfx.GoogleMapView;
 import javafx.animation.TranslateTransition;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.util.Duration;
 
 import java.net.URL;
@@ -19,19 +25,39 @@ public class mainStageController implements Initializable{
 
     @FXML
     private Button filterButton;
+    
+    @FXML
+    private Button addQueryButton;
 
     @FXML
     private AnchorPane navList;
+
+    @FXML
+    private ListView<Query> queryList = new ListView<>(FXCollections.observableArrayList());
+
+    @FXML
+    private ColorPicker picker;
+    private Color pickedColor;
+
+    @FXML
+    private TextField pickedSpecies;
+
 
     private TranslateTransition openNav;
     private TranslateTransition closeNav;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        queryList.setEditable(true);
         filterButton.setOnAction(this::openFilterPanel);
+        addQueryButton.setOnAction(this::addNewQuery);
         openNav=new TranslateTransition(new Duration(350), navList);
         openNav.setToX(0);
         closeNav=new TranslateTransition(new Duration(350), navList);
+
+        picker.setOnAction(e -> {pickedColor = picker.getValue();});
+
     }
 
     @FXML
@@ -49,5 +75,9 @@ public class mainStageController implements Initializable{
         this.mainContent.getChildren().add(map);
     }
 
-
+    @FXML
+    public void addNewQuery(ActionEvent actionEvent) {
+        Query q = new Query(pickedSpecies.getText(), pickedColor);
+        queryList.getItems().add(q);
+    }
 }
